7371f99ae620a97203f9d19bf7502165d492f7cf
Branca Scannapieco Della Coletta <brancasdc@gmail.com>
2022-06-09 11:25:25 -0300

    geocoder

--- app/javascript/controllers/geocoder_controller.js
+++ app/javascript/controllers/geocoder_controller.js
@@ -1,28 +1,31 @@
-// import { Controller } from "@hotwired/stimulus"
-// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
-// export default class extends Controller {
-//   static values = { apiKey: String }
+export default class extends Controller {
+  static values = { apiKey: String }
 
-//   static targets = ["child_address"]
-//   // adicionar targets dos outros endereços
+  static targets = ["child_address","home_address", "school_adress"]
 
-//   connect() {
-//     this.geocoder = new MapboxGeocoder({
-//       accessToken: this.apiKeyValue,
-//       types: "country,region,place,postcode,locality,neighborhood,address"
-//     });
-//     this.geocoder.addTo(this.element)
+  connect() {
+    this.geocoder = new MapboxGeocoder({
+      accessToken: this.apiKeyValue,
+      types: "country,region,place,postcode,locality,neighborhood,address"
+    });
+    this.geocoder.addTo(this.element)
 
-//     this.geocoder.on("result", event => this.#setInputValue(event))
-//     this.geocoder.on("clear", () => this.#clearInputValue())
-//   }
+    this.geocoder.on("result", event => this.#setInputValue(event))
+    this.geocoder.on("clear", () => this.#clearInputValue())
+  }
 
-//   #setInputValue(event) {
-//     this.addressTarget.value = event.result["place_name"]
-//   }
+  #setInputValue(event) {
+    this.child_addressTarget.value = event.result["place_name"]
+    this.home_addressTarget.value = event.result["place_name"]
+    this.school_addressTarget.value = event.result["place_name"]
+  }
 
-//   #clearInputValue() {
-//     this.addressTarget.value = ""
-//   }
-// }
+  #clearInputValue() {
+    this.child_addressTarget.value = ""
+    this.home_addressTarget.value = ""
+    this.school_addressTarget.value = ""
+  }
+}
