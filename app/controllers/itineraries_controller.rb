030d267207159c37731f6e6056d08716787c4dfd
Matheus Fortini <matheusfortini7@gmail.com>
2022-06-10 16:05:56 -0300

    Criação da rota de acordo com a sequência

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -2,24 +2,27 @@ class ItinerariesController < ApplicationController
   def show
     @itinerary = Itinerary.find(params[:id])
     @markers = @itinerary.students.order(:sequence).map do |student|
-      {lat: student.latitude_child, lng: student.longitude_child}
+      { lat: student.latitude_child, lng: student.longitude_child }
     end
 
-    @points = {origin: {
+    @points = {
+      origin: {
         lat: @itinerary.user.latitude_home,
         lng: @itinerary.user.longitude_home
       },
 
       waypoints: @itinerary.students.order(:sequence).map do |student|
-        {sequence: student.sequence,
-        lat: student.latitude_child,
-        lng: student.longitude_child
+        {
+          sequence: student.sequence,
+          lat: student.latitude_child,
+          lng: student.longitude_child
         }
       end,
 
-        destination: {
-          lat: @itinerary.user.latitude_school,
-          lng: @itinerary.user.longitude_school
-        }}
+      destination: {
+        lat: @itinerary.user.latitude_school,
+        lng: @itinerary.user.longitude_school
+      }
+    }
   end
 end
