4a9c2a558efe29875310f445eb00b5f1a9b46822
Bruno Shingai <brunoshingai@gmail.com>
2022-06-13 12:01:55 -0300

    salvando para n perder as modificações

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,11 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
+  def welcome
+    user = User.first
+
+    UserMailer.with(student: student).welcome
+  end
+
+end
